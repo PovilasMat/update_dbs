f22b4a6c63c83f6c3affb5a9065fe300d4252c24
PovilasMat <povilas.matusevicius@gmail.com>
2025-03-04 23:36:38 +0100

    Update update_databases.sh

--- update_databases.sh
+++ update_databases.sh
@@ -1,14 +1,14 @@
 #!/usr/bash
-#conda activate BTG_finders
+
 mkdir -p /home/gebt/BTG/Day8_pipelines/
 
 mkdir -p /home/gebt/BTG/Day8_pipelines/dbs/
 cd /home/gebt/BTG/Day8_pipelines/dbs/
 
-git clone https://bitbucket.org/genomicepidemiology/plasmidfinder_db.git
+git clone https://bitbucket.org/genomicepidemiology/resfinder_db.git
 
-cd plasmidfinder_db
-PLASMID_DB=$(pwd)
+cd resfinder_db
+
 micromamba run -n BTG_resistance python3 INSTALL.py kma_index
 
 micromamba run -n BTG_resistance amrfinder --update
